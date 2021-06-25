dc691527760493de3308919ba9c0d75dad4d38d3
FrancescoPeace <86018837+FrancescoPeace@users.noreply.github.com>
2021-06-25 22:15:59 +0200

    Add files via upload

--- Java.js
+++ Java.js
@@ -5,15 +5,27 @@ function submit(){
     var email = document.getElementById('email').value;
     var message = document.getElementById('message').value;
 
-    var table = document.getElementById('table');
-    var newRow = table.insertRow(1);
-    cel1 = newRow.insertCell(0);
-    cel2 = newRow.insertCell(1);
-    cel3 = newRow.insertCell(2);
-
-    cel1.innerHTML= name;
-    cel2.innerHTML= email;
-    cel3.innerHTML= message;
+    if (name!=="" && email!=="" && message!=="") {
+    
+        var table = document.getElementById('table');
+        var newRow = table.insertRow(1);
+        cel1 = newRow.insertCell(0);
+        cel2 = newRow.insertCell(1);
+        cel3 = newRow.insertCell(2);
+    
+        cel1.innerHTML= name;
+        cel2.innerHTML= email;
+        cel3.innerHTML= message;
+    
+        document.getElementById('name').value = "";
+        document.getElementById('email').value = "";
+        document.getElementById('message').value = "" 
+    }
+    
+    else{
+        return alert("Compila tutti i campi")
+    }
+    
 
 }
 
